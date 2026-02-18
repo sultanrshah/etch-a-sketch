7677f76f01eeeadf03d5a01aa66d76982f8ab846
sultan <sultanrshah@tutamail.com>
2026-02-18 12:44:48 -0600

    create grid environment with 16x16 divs

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const tempHold = document.createDocumentFragment();
 
 const numberOfDivs = 256;
 
-container.style.setProperty('--grid-rows', 16);
-container.style.setProperty('--grid-cols', 16);
-
 for (let i = 0; i < 256; i++) {
     const cell = document.createElement("div");
     cell.classList.add("grid-item");
