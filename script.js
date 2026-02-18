df776cf944a8a82278c3f9922b342d105a437086
sultan <sultanrshah@tutamail.com>
2026-02-18 13:16:30 -0600

    Make width and heigth changable by user input

--- script.js
+++ script.js
@@ -2,14 +2,35 @@ const container = document.getElementById("container");
 
 const tempHold = document.createDocumentFragment();
 
-const sideLength = 16;
+function makeGrid(sideLength) {
+    container.replaceChildren();
+    
+    numCells = sideLength * sideLength;
 
-numCells = sideLength * sideLength;
+    for (let i = 0; i < numCells; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-item");
+        cell.style.width = `calc(100% / ${sideLength})`;
+        cell.style.height = `calc(100% / ${sideLength})`;
+        cell.addEventListener("mouseover", function () {
+            this.style.backgroundColor = "black";
+        });
 
-for (let i = 0; i < numCells; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("grid-item");
-    tempHold.appendChild(cell);
+        tempHold.appendChild(cell);
+
+    }
+
+    container.appendChild(tempHold);
 }
 
-container.appendChild(tempHold);
+const button = document.getElementById("newGrid");
+
+button.addEventListener("click", () => {
+    let input = prompt("How many pixels wide (<100)?", 16);
+
+    if (!(input > 0) || !(input < 100)) {
+        alert("Try again");
+    } else {
+        makeGrid(input);
+    }
+})
